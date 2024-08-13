de9ff8cbd8705a98e61703c252f1a37baa2ca65b
marvd <marvinzcalderon@gmail.com>
2024-08-12 21:46:18 -0600

    se agregarlon los cambios del otro proyecto (insertar y main, en este caso arreglos)

--- src/fes/aragon/Arreglos.java
+++ src/fes/aragon/Arreglos.java
@@ -1,4 +1,12 @@
 package fes.aragon;
 
+import fes.aragon.modelo.insertar;
+
 public class Arreglos {
+    public static void main(String[] args) {
+        System.out.println("hola");
+        int[] arreglo=new int[10];
+        insertar insertar = new insertar(20,arreglo);
+
+    }
 }
---
+++ src/fes/aragon/modelo/insertar.java
@@ -0,0 +1,21 @@
+package fes.aragon.modelo;
+
+public class insertar {
+    private int indice;
+
+    public insertar(int valor, int[] a) {
+        if(!validarLongitud(a)){
+            a[indice]=valor;
+            indice++;
+        }
+    }
+
+
+    private boolean validarLongitud(int[]a){
+        boolean limite=false;
+        if(indice==a.length){
+            limite=true;
+        }
+        return limite;
+    }
+}
