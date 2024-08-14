5ac5a63be24458dfaea7ec24d702ae9dd08c8477
marvd <marvinzcalderon@gmail.com>
2024-08-13 19:03:10 -0600

    se agrego suprimir con seguridad

--- src/fes/aragon/modelo/Cliente.java
+++ src/fes/aragon/modelo/Cliente.java
@@ -5,72 +5,75 @@ public class Cliente {
     private int posi;
 
 
-public void cliente(){
+    public void cliente() {
 
-}
+    }
 
     public void insertar(int valor, int[] a) {
         System.out.println("insertar funciona");
-        if(!validarLongitud(a)){
-            a[indice]=valor;
-            System.out.println(valor+"<---- valor agregado");
+        if (!validarLongitud(a)) {
+            a[indice] = valor;
+            System.out.println(valor + "<---- valor agregado");
             indice++;
         }
     }
 
 
-    private boolean validarLongitud(int[]a){
-        boolean limite=false;
+    private boolean validarLongitud(int[] a) {
+        boolean limite = false;
         System.out.println("validar funciona");
-        if(indice==a.length){
-            limite=true;
+        if (indice == a.length) {
+            limite = true;
         }
         return limite;
     }
 
-    public int localizar(int[]a, int x){
-        int pos=0;
+    public int localizar(int[] a, int x) {
+        int pos = 0;
         System.out.println("localizar funciona");
-        for(int i=0; i<a.length;i++){
-            if(a[i]==x){
-                pos=i;
-                System.out.println(pos+ "<---- posicion");
-            }
-            else{
-                pos=(-1);
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == x) {
+                pos = i;
+                System.out.println(pos + "<---- posicion");
+            } else {
+                pos = (-1);
             }
         }
         return pos;
     }
 
-    public boolean validarPosicion(int[]a){
-        boolean posicion=false;
-        if(posi<a.length){
-            posicion=true;
+    public boolean validarPosicion(int[] a) {
+        boolean posicion = false;
+        if (posi < a.length) {
+            posicion = true;
         }
         return posicion;
     }
-    public int recuperar(int[]a, int p){
+
+    public int recuperar(int[] a, int p) {
         int val = 0;
-        if(validarPosicion(a)) {
+        if (validarPosicion(a)) {
             System.out.println("recuperar funciona");
-            for(int i=0; i<a.length;i++){
-                if(p==i){
-                    val=a[p];
+            for (int i = 0; i < a.length; i++) {
+                if (p == i) {
+                    val = a[p];
 
                 }
             }
         }
-        System.out.println(val+ "<---- Recuperado");
+        System.out.println(val + "<---- Recuperado");
         return val;
     }
-    public void suprimir(int[]a,int p){
-        System.out.println(a[p]+"<----antes de restablecer");
-        for (int i = 0; i < a.length; i++) {
-                if(i==p){
-                    a[i]=0;
-                    System.out.println(a[i]+"<----Se restablece a 0");
+
+    public void suprimir(int[] a, int p) {
+        System.out.println(a[p] + "<----antes de restablecer");
+        if (validarPosicion(a)) {
+            for (int i = 0; i < a.length; i++) {
+                if (i == p) {
+                    a[i] = 0;
+                    System.out.println(a[i] + "<----Se restablece a 0");
                 }
+            }
         }
     }
 }
