3c5403aea29d8cddf18de244410afef48bedea8f
marvd <marvinzcalderon@gmail.com>
2024-08-15 17:35:30 -0600

    IndiceFueraDeRango arreglado en la clase Arreglo.java

--- src/fes/aragon/estatico/Arreglo.java
+++ src/fes/aragon/estatico/Arreglo.java
@@ -4,6 +4,7 @@ import fes.aragon.excep.IndiceFueraDeRango;
 
 /*
 Clase que tiene funciones para utilizar arreglos de tipo E
+@author Bautista Solis Juan Pedro, Calderon Almanza Marvin Daniel
  */
 public class Arreglo<E> {
     private int indice = 0;
@@ -18,9 +19,9 @@ public class Arreglo<E> {
     Método que inserta un valor de tipo E en el arreglo de manera consecutiva
 
     @param x es el parametro que se recibe el cual se agregara a la lista
-    @throws IndiceFueraDeRango exepción que se activa cuando se excede el indice
+    @throws IndiceFueraDeRango exepción que se activa cuando se excede el índice
      */
-    public void insertar(E x)/*throws  IndiceFueraDeRango */ {
+    public void insertar(E x)throws  IndiceFueraDeRango {
         if (indice < l.length) {
             l[indice] = x;
             indice++;
@@ -30,7 +31,7 @@ public class Arreglo<E> {
     }
 
     /*
-Método que localiza un valor de tipo E del arreglo y retorna el indice
+Método que localiza un valor de tipo E del arreglo y retorna el índice
 
 @param x es el valor de tipo E a buscar en el arreglo
 @return se retorna -1 si el valor no se encuentra en la lista, en caso contrario se retorna el indice
@@ -46,27 +47,26 @@ Método que localiza un valor de tipo E del arreglo y retorna el indice
 
 
     /*
-Método que recupera un valor de tipo E del arreglo mediante el indice, retorna el valor de tipo E
+Método que recupera un valor de tipo E del arreglo mediante el índice, retorna el valor de tipo E
 
-@param x indica el indice del elemento a recuperar
-@return se retorna el valor de tipo E correspondiente al indice
-@trows IndiceFueraDeRango exepción que se activa cuando el indice x esta fuera de los rangos del arreglo
+@param x índica el índice del elemento a recuperar
+@return se retorna el valor de tipo E correspondiente al índice
+@trows IndiceFueraDeRango exepción que se activa cuando el índice x está fuera de los rangos del arreglo
  */
-    public E recupera(int x)/*throws IndiceFueraDeRango */ {
+    public E recupera(int x) throws IndiceFueraDeRango{
         if (x < l.length || x < 0) {
             throw new IndiceFueraDeRango("Indice fuera de rango");
         } else {
             final E e = (E) l[x];
             return e;
         }
-        return null;
     }
 
     /*
-Método que elimina un valor de tipo E del arreglo mediante el indice indicado
+Método que elimina un valor de tipo E del arreglo mediante el índice indicado
 
-@param x indica el indice del elemento a eliminar
-@trows IndiceFueraDeRango exepción que se activa cuando el indice x esta fuera de los rangos del arreglo
+@param x índica el índice del elemento a eliminar
+@trows IndiceFueraDeRango exepción que se activa cuando el índice x está fuera de los rangos del arreglo
  */
     public void suprime(int x) throws IndiceFueraDeRango{
         if (x > l.length || x < 0) {
@@ -77,13 +77,13 @@ Método que elimina un valor de tipo E del arreglo mediante el indice indicado
     }
 
     /*
-    Metodo que recupera el valor E siguiente al indice indicado
+    Metodo que recupera el valor E siguiente al índice indicado
 
-    @param x indica el valor del indice
-    @return se retorna el valor E correspondiente a la una posicion siguiente del indice indicado
-    @trows IndiceFueraDeRango exepción que se activa cuando el indice x esta fuera de los rangos del arreglo
+    @param x índica el valor del índice
+    @return se retorna el valor E correspondiente a la una posicion siguiente del índice indicado
+    @trows IndiceFueraDeRango exepción que se activa cuando el índice x está fuera de los rangos del arreglo
      */
-    public E siguiente(int x){
+    public E siguiente(int x)throws IndiceFueraDeRango{
         if (x > l.length || x <= 0) {
             throw new IndiceFueraDeRango("indice fuera de rango");
         }
@@ -100,13 +100,13 @@ Método que elimina un valor de tipo E del arreglo mediante el indice indicado
     }
 
     /*
-    Metodo que recupera el valor E anterior al indice indicado
+    Metodo que recupera el valor E anterior al índice indicado
 
-    @param x indica el valor del indice
-    @return se retorna el valor E correspondiente a la una posicion anterior del indice indicado
-    @trows IndiceFueraDeRango exepción que se activa cuando el indice x esta fuera de los rangos del arreglo o se quiere recuperar el valor anterior de la posicion 0 del arreglo
+    @param x índica el valor del índice
+    @return se retorna el valor E correspondiente a la una posicion anterior del índice indicado
+    @trows IndiceFueraDeRango exepción que se activa cuando el índice x está fuera de los rangos del arreglo o se quiere recuperar el valor anterior de la posicion 0 del arreglo
      */
-    public E anterior(int x) {
+    public E anterior(int x)throws IndiceFueraDeRango {
         if (x > l.length || x <= 0) {
             throw new IndiceFueraDeRango("indice fuera de rango");
         }
