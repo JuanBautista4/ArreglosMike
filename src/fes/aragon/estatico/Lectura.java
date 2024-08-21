a6aa4c05b47623841453a8562ce67f812050806f
marvd <marvinzcalderon@gmail.com>
2024-08-20 19:13:06 -0600

    ya los guarda en el arreglo

--- src/fes/aragon/estatico/Lectura.java
+++ src/fes/aragon/estatico/Lectura.java
@@ -1,26 +1,36 @@
 package fes.aragon.estatico;
+
 import fes.aragon.excep.IndiceFueraDeRango;
-import fes.aragon.estatico.Arreglo;
+
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.BufferedReader;
 
 public class Lectura {
-    public static void main(String[] args) throws IndiceFueraDeRango {
-        String archivoCSV = "ruta/del/archivo.csv";
+    public static void main(String[] args){
+        String archivoCSV = "/home/marvd/Descargas/floats_two_decimals.csv";
         String linea = "";
         String separador = ",";
+        Arreglo<Double> generico=new Arreglo<>(300);
 
         try (BufferedReader br = new BufferedReader(new FileReader(archivoCSV))) {
             while ((linea = br.readLine()) != null) {
                 // Usar el separador para dividir las columnas
                 String[] datos = linea.split(separador);
 
-                // Imprimir o procesar los datos
-                System.out.println("ID: " + datos[0] + ", Nombre: " + datos[1] + ", Edad: " + datos[2]);
+                for (String valor : datos) {
+                    Double dato = Double.parseDouble(valor);
+                    generico.insertar(dato);
+           }
+                generico.imprime();
+
+
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
+        } catch (IndiceFueraDeRango e) {
+            throw new RuntimeException(e);
         }
     }
+
 }
