1b97b9572d0aaa1df6f2ea92ce5dc1542d71d608
Juanpi <juanpedrobautista78@gmail.com>
2024-08-20 21:28:44 -0600

    ahora si ya jala el mayor

--- src/fes/aragon/estatico/Lectura.java
+++ src/fes/aragon/estatico/Lectura.java
@@ -33,7 +33,7 @@ public class Lectura {
                 Double min=burbujaMinimo(generico);
                 System.out.println("Val Min:"+min);
                 Double max=burbujaMaximo(generico);
-                System.out.println("Val Max"+max );
+                System.out.println("Val Max"+max);
 
             }
         } catch (IOException e) {
@@ -59,12 +59,13 @@ public class Lectura {
     public static <T extends Comparable<T>> T burbujaMaximo(Arreglo<T> arr) throws IndiceFueraDeRango {
         int n = arr.tam();
         for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - 1 - i; j++) {
-                if (arr.recupera(j).compareTo(arr.recupera(j + 1)) < 0) {  // Cambio de comparación
-                    arr.intercambiar(j, j + 1);
+            for (int j = n - 1; j > i; j--) {  // Recorre desde el final hacia el inicio
+                if (arr.recupera(j).compareTo(arr.recupera(j - 1)) > 0) {
+                    arr.intercambiar(j, j - 1);
                 }
             }
         }
-        return arr.recupera(n - 1); // El valor más grande estará en la última posición
+        return arr.recupera(0);  // El valor más grande estará en la primera posición
+
     }
 }
